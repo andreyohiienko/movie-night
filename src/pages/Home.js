90d134576984f74c1d4ff3a9e5cc7fec33241b7f
Andrii Ohiienko <andreyohiienko@gmail.com>
2022-07-15 15:51:19 -0700

    Add like functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
 import { getImageUrl } from "../utils";
 import { Link } from "react-router-dom";
-import { useAxiosGet } from "../hooks";
+import { useAxiosGet, useLocalStorage } from "../hooks";
 import Icon from "../icon";
-import { useState } from "react";
 
 const Home = () => {
-  const [state, setState] = useState(false);
+  const [likeds, setLikeds] = useLocalStorage("likeds", []);
   const { data, loading } = useAxiosGet("/movie/popular");
+
+  const likedIds = likeds?.map((movie) => movie.id);
+
+  const onLike = (movie) => {
+    setLikeds((prev) =>
+      likedIds.includes(movie.id)
+        ? prev.filter((m) => m.id !== movie.id)
+        : [...prev, movie],
+    );
+  };
 
   return (
     <div className="mx-auto container px-4">
@@ -20,27 +29,29 @@ const Home = () => {
             <Link to="/liked">Liked</Link>
           </li>
         </ul>
-
         {loading ? <Icon type="spinner" /> : null}
         {data?.results.length ? (
           <div className="grid grid-cols-4 gap-4">
-            {data.results.map(({ id, title, poster_path: posterPath }) => (
-              <div key={id}>
-                <div className="relative">
-                  <img
-                    className="m-0"
-                    src={getImageUrl(posterPath)}
-                    alt={title}
-                  />
-                  <Icon
-                    onClick={() => setState(!state)}
-                    className="absolute bottom-2 right-2 z-10 cursor-pointer"
-                    type={state ? "liked" : "unliked"}
-                  />
+            {data.results.map((movie) => {
+              const { id, title, poster_path: posterPath } = movie;
+              return (
+                <div key={id}>
+                  <div className="relative">
+                    <img
+                      className="m-0"
+                      src={getImageUrl(posterPath)}
+                      alt={title}
+                    />
+                    <Icon
+                      onClick={() => onLike(movie)}
+                      className="absolute bottom-2 right-2 z-10 cursor-pointer"
+                      type={likedIds.includes(id) ? "liked" : "unliked"}
+                    />
+                  </div>
+                  <h3>{title}</h3>
                 </div>
-                <h3>{title}</h3>
-              </div>
-            ))}
+              );
+            })}
           </div>
         ) : null}
       </div>
