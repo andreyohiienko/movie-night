f05fc1900a5191daf4a9b5541cc9b1756590d589
Andrii Ohiienko <andreyohiienko@gmail.com>
2022-07-15 21:17:49 -0700

    Refactor links

--- src/HOCs/Main.js
+++ src/HOCs/Main.js
@@ -1,34 +1,35 @@
 import { NavLink, Outlet } from "react-router-dom";
 
 const Main = () => {
+  const links = [
+    {
+      to: "/",
+      title: "Home",
+    },
+    {
+      to: "/liked",
+      title: "Liked",
+    },
+  ];
+
   return (
     <div className="mx-auto container px-4">
       <div className="prose max-w-none text-center">
         <ul className="list-none pl-0 flex mt-4">
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive
-                  ? "bg-green-500 hover:bg-green-600  active:bg-green-700 px-5 py-2 text-sm leading-5 rounded-full text-white"
-                  : ""
-              }
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/liked"
-              className={({ isActive }) =>
-                isActive
-                  ? "bg-green-500 hover:bg-green-600  active:bg-green-700 px-5 py-2 text-sm leading-5 rounded-full text-white"
-                  : ""
-              }
-            >
-              Liked
-            </NavLink>
-          </li>
+          {links.map(({ to, title }) => (
+            <li key={title}>
+              <NavLink
+                to={to}
+                className={({ isActive }) =>
+                  isActive
+                    ? "bg-green-500 hover:bg-green-600  active:bg-green-700 px-5 py-2 text-sm leading-5 rounded-full text-white"
+                    : ""
+                }
+              >
+                {title}
+              </NavLink>
+            </li>
+          ))}
         </ul>
         <Outlet />
       </div>
